31bf1e3ed9e7aaaeb787ba9417e91c894d78e25b
Praneet Kumar <praneetk96@gmail.com>
2026-03-18 19:24:49 +0530

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,3 @@
 require("config.core")
 require("config.lazy")
-
-vim.api.nvim_exec([[
-  augroup filetemplates
-    autocmd!
-    autocmd bufnewfile *.cpp 0r ~/.config/nvim/templates/main.cpp | call v:lua.templatesubstitute()
-    autocmd bufnewfile *.py  0r ~/.config/nvim/templates/main.py  | call v:lua.templatesubstitute()
-    autocmd bufnewfile *.sh  0r ~/.config/nvim/templates/main.sh  | call v:lua.templatesubstitute()
-  augroup end
-]], false)
-
-function templatesubstitute()
-  local substitutions = {
-    ["%upperfile%"] = string.upper(vim.fn.expand("%:t:r")), -- filename without extension, in all caps
-    ["%author%"] = "Praneet Kumar",  -- replace with your name
-    ["%date%"] = os.date("%d-%m-%Y")
-  }
-
-  for pattern, value in pairs(substitutions) do
-    vim.cmd(string.format("%%s/%s/%s/ge", pattern, value))
-  end
-end
+require("config.autocmds")
